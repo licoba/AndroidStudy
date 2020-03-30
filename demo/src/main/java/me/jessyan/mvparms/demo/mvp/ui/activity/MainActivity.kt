f2644286429e331b8334ceda7f4f8980aec63123
licoba <v_lijwli@tencent.com>
2020-03-30 15:15:00 +0800

    添加kotlin的主页面

---
+++ demo/src/main/java/me/jessyan/mvparms/demo/mvp/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package me.jessyan.mvparms.demo.mvp.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.jessyan.mvparms.demo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
